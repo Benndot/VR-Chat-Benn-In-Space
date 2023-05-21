0243017330c6fc800844289a995659c3c551e592
Benndot <ben.diraly@gmail.com>
2023-05-20 20:33:09 -0400

    initial commit

---
+++ Assets/MyUdonStuff/FirstScript.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+namespace benndot.tutorial
+{
+
+    public class FirstScript : UdonSharpBehaviour
+    {
+        
+        public GameObject firstCube;
+        public float floatVar;
+
+        public Text textVar;
+        public InputField inputVar;
+
+        void Start()
+        {
+            
+        }
+    }
+}
+
